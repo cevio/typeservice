c58a259b18c747cddae60593ebd20c599663e24f
沈赟杰 <shenyunjie@znrank.com>
2022-01-12 22:20:00 +0800

    update

--- packages/http/src/decorators/HTTPCookie.ts
+++ packages/http/src/decorators/HTTPCookie.ts
@@ -2,17 +2,8 @@ import * as Cookie from 'cookies';
 import { ParameterMetaCreator } from '@typeservice/decorator';
 import { Context } from 'koa';
 
-export type TCookieResolve = {
-  get: (key: string, opt?: Cookie.GetOption) => string;
-  set: (key: string, value: string, opt?: Cookie.SetOption) => Cookie;
-}
+export type TCookie = Cookie;
 
-export function HTTPCookie(keys: string[] = ['cookie']) {
-  return ParameterMetaCreator.define((ctx: Context) => {
-    const cookies = new Cookie(ctx.req, ctx.res, { keys });
-    return {
-      get: (key: string, opt?: Cookie.GetOption) => cookies.get(key, opt),
-      set: (key: string, value: string, opt?: Cookie.SetOption) => cookies.set(key, value, opt),
-    } as TCookieResolve;
-  })
+export function HTTPCookie() {
+  return ParameterMetaCreator.define((ctx: Context) => ctx.cookies);
 }
